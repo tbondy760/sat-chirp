670929c576e905f36562b6a23ae6f7ac45fcaea1
Terry Bondy, VA3TYB <terry.bondy.fun@gmail.com>
2019-12-29 19:21:47 +0000

    WIP

---
+++ gitconfig.sh
@@ -0,0 +1,5 @@
+    8  git config user.email "terry.bondy@gmail.com"
+    9  git config user.name "Terry Bondy, VA3TYB"
+   10  git config credential.helper 'cache --timeout=30000'
+   11  history | tail -4
+   12  history | tail -5 > gitconfig.sh
